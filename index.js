54e3408c87af9a0606776b27072989c54299408d
Oliver Labawan <ol.labawan8199@gmail.com>
2023-12-18 12:46:09 +0800

    additional changes

--- index.js
+++ index.js
@@ -1,29 +1,148 @@
-let navbar = document.querySelector('.navbar');
+let listProductHTML = document.querySelector('.listProduct');
+let listCartHTML = document.querySelector('.listCart');
+let iconCart = document.querySelector('.icon-cart');
+let iconCartSpan = document.querySelector('.icon-cart span');
+let body = document.querySelector('body');
+let closeCart = document.querySelector('.close');
+let products = [];
+let cart = [];
 
-document.querySelector('.menu-btn').onclick = () => {
-    navbar.classList.toggle('active');
-    cartItem.classList.remove('active');
-    searchForm.classList.remove('active');
+iconCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
+})
+closeCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
+})
+
+const addDataToHTML = () =>{
+
+
+    //add new data to html
+    if(products.length > 0)
+    {
+        products.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.dataset.id = product.id;
+            newProduct.classList.add('item');
+            newProduct.innerHTML = 
+            `<img src="${product.image}" alt="">
+            <h2>${product.name}</h2>
+            <div class = "price">₱${product.price}</div>
+            <button class = "addCart"> Add to Cart </button>`;
+            listProductHTML.appendChild(newProduct);
+        });
+    }
 }
 
-let cartItem = document.querySelector('.cart-items-sec');
+listProductHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('addCart')){
+        let id_product = positionClick.parentElement.dataset.id;
+        addToCart(id_product);
+    }
+})
 
-document.querySelector('.cart-btn').onclick = () => {
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active'); 
-    searchForm.classList.remove('active');
+const addToCart = (product_id) => {
+    let positionThisProductInCart = cart.findIndex((value) => value.product_id == product_id);
+    if(cart.length <= 0){
+        cart = [{
+            product_id: product_id,
+            quantity:1
+        }];
+    }
+    else if(positionThisProductInCart < 0){
+        cart.push({
+            product_id : product_id ,
+            quantity : 1
+        });
+    }
+    else{
+        cart[positionThisProductInCart].quantity = cart[positionThisProductInCart].quantity + 1;
+    }
+    addCartToHTML();
 }
 
-let searchForm = document.querySelector('.search-form');
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = '';
+    let totalQuantity = 0;
+    if(cart.length > 0){
+        cart.forEach(item => {
+            totalQuantity = totalQuantity + item.quantity;
+            let newItem = document.createElement('div');
+            newItem.classList.add('item');
+            newItem.dataset.id = item.product_id;
 
-document.querySelector('.search-btn').onclick = () => {
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
+            let positionProduct = products.findIndex((value) => value.id == item.product_id);
+            let info = products[positionProduct];
+            listCartHTML.appendChild(newItem);
+            newItem.innerHTML = `
+            <div class = "image">
+                <img src = "${info.image}">
+            </div>
+            <div class="name">
+            ${info.name}
+            </div>
+            <div class = "totalPrice">₱${info.price * item.quantity} </div>
+            <div class = "quantity">
+                <span class = "minus"> - </span>
+                <span>${item.quantity}</span>
+                <span class = "plus"> + </span>
+            </div>
+            `;
+        })
+    }
+
+    iconCartSpan.innerText = totalQuantity;
 }
 
-window.onscroll = () => {
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
-    searchForm.classList.remove('active');
+listCartHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('minus') || positionClick.classList.contains('plus')){
+        let product_id = positionClick.parentElement.parentElement.dataset.id;
+        let type = 'minus';
+        if(positionClick.classList.contains('plus')){
+            type = 'plus'
+        }
+        changeQuantityCart(product_id, type);
+    }
+})
+
+const changeQuantityCart = (product_id, type) => {
+    let positionItemInCart = cart.findIndex((value) => value.product_id == product_id);
+    if(positionItemInCart >= 0){
+        let info = cart[positionItemInCart];
+        switch(type) {
+            case 'plus':
+                cart[positionItemInCart].quantity = cart[positionItemInCart].quantity + 1;
+                break;
+            
+            default:
+                let changeQuantity = cart[positionItemInCart].quantity - 1;
+                if(changeQuantity > 0){
+                    cart[positionItemInCart].quantity = changeQuantity;
+                }
+                else{
+                    cart.splice(positionItemInCart, 1);
+                }
+                break;
+        }
+    }
+    addCartToHTML();
 }
+
+
+const initApp = () => {
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        addDataToHTML();
+
+        if(localStorage.getItem('cart')){
+            cart = JSON.parse(localStorage.getItem('cart'));
+            addCartToHTML();
+        }
+    })
+}
+
+initApp();
